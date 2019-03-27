ef943235347eab487528a99a6d985747f3287e30
Hannah Coffman <hcoffman7@gmail.com>
2019-03-27 22:39:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,13 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let tracker = 0
 
-function init() {
+function init() {}
   
 function onKeyDown(event) {
   if (event.keyCode === code[tracker]) {
     tracker++ 
   } else {
-  // if they get it wrong 
   tracker = 0
   }
  if (tracker === 10) {
@@ -16,12 +15,10 @@ function onKeyDown(event) {
 }
 }
 
-}
  // event //callback (trigger something)
 document.addEventListener('keydown', onKeyDown)
                            
 init()
-
 
 //  let key = parseInt(event.which)
 //  console.log(event.which);
